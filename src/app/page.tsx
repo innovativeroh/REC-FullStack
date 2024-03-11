1251e9291d1be58fd6903d649f96ba01481c952b
Rohan Sidhu <rohan.sidhu@outlook.com>
2024-03-12 01:58:33 +0530

    Fuck You

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-
+import Header from "../components/Header/Header";
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className='m-auto max-w-[1000px] bg-black'>
+        Hello World
+    </div>
+  )
+}
+
+export default Header
